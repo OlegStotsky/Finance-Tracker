02c45c9139d2d66f9ba6162166f0836c89355f0c
Oleg Stotsky <ostotsky@gmail.com>
2018-08-06 17:35:45 +0000

    Refactor StocksController#search

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,17 +1,17 @@
 class StocksController < ApplicationController
     def search
-        if params[:stock].present?
-            begin
-                @stock = Stock.new_from_lookup(params[:stock])
-                respond_to do |format|
-                    format.js { render partial: 'users/result' }
-                end
-            rescue NilStockException => e
-                flash[:danger] = e.message
-                redirect_to my_portfolio_path
-            end
-        else
+        unless params[:stock].present?
             flash[:danger] = "You have entered empty search string"
+            redirect_to my_portfolio_path
+            return
+        end
+        begin
+            @stock = Stock.new_from_lookup(params[:stock])
+            respond_to do |format|
+                format.js { render partial: 'users/result' }
+            end
+        rescue NilStockException => e
+            flash[:danger] = e.message
             redirect_to my_portfolio_path
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/my_portfolio'
-
   devise_for :users
   
   root 'pages#index'
